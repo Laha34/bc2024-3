7b50505b97243a07fe032734cafd01872fbc33c3
Denys bc2024-3 <denys.lahoida@gmail.com>
2024-10-27 11:58:02 +0200

    Add index.js

--- index.js
+++ index.js
@@ -1,47 +1,49 @@
-// index.js
 const fs = require('fs');
 const path = require('path');
 const { Command } = require('commander');
 const program = new Command();
 
-// Налаштування параметрів командного рядка
 program
   .option('-i, --input <path>', 'input file path (required)')
   .option('-o, --output <path>', 'output file path (optional)')
   .option('-d, --display', 'display the result in the console (optional)');
 
-// Розбір аргументів командного рядка
 program.parse(process.argv);
 const options = program.opts();
 
-// Перевірка наявності обов'язкового параметру `--input`
 if (!options.input) {
   console.error('Please, specify input file');
   process.exit(1);
 }
 
-// Отримання абсолютного шляху до файлу
 const inputFilePath = path.resolve(options.input);
 
-// Перевірка існування файлу за вказаним шляхом
 if (!fs.existsSync(inputFilePath)) {
   console.error('Cannot find input file');
   process.exit(1);
 }
 
-// Зчитування вмісту вхідного файлу
-const inputData = fs.readFileSync(inputFilePath, 'utf-8');
-
-// Перевірка, чи потрібно виводити результат у консоль
-if (options.display) {
-  console.log('Data from input file:', inputData);
+function getMaxCurrencyRate(inputFile) {
+  try {
+    const data = fs.readFileSync(inputFile, 'utf-8');
+    const ratesArray = JSON.parse(data); 
+    const maxRate = Math.max(...ratesArray.map(item => item.rate)); 
+    return `Максимальний курс:${maxRate}`;
+  } catch (error) {
+    console.error('Помилка при обробці файлу:', error);
+    process.exit(1);
+  }
 }
 
-// Перевірка, чи задано шлях для виведення результату у файл
+const result = getMaxCurrencyRate(inputFilePath);
+
+if (options.display) {
+  console.log(result);
+}
+
 if (options.output) {
   const outputFilePath = path.resolve(options.output);
-  fs.writeFileSync(outputFilePath, inputData, 'utf-8');
+  fs.writeFileSync(outputFilePath, result, 'utf-8');
   console.log(`Data has been written to ${outputFilePath}`);
 }
 
-// Якщо не задано параметри `--output` або `--display`, програма нічого не виводить
